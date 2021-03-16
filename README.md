c59f0b22e8dde3b3b3551167f44b41f25111dd15
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 14:36:35 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/jonathanmaron/ctw-middleware-generated-by/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/jonathanmaron/ctw-middleware-generated-by/?branch=master)
 [![Latest Stable Version](https://poser.pugx.org/ctw/ctw-middleware-generated-by/v/stable)](https://packagist.org/packages/ctw/ctw-middleware-generated-by)
 
-
 ```bash
 $ composer require ctw/ctw-middleware-generated-by
 ```
--- test/GeneratedByMiddlewareTest.php
+++ test/GeneratedByMiddlewareTest.php
@@ -28,6 +28,18 @@ class GeneratedByMiddlewareTest extends AbstractCase
         $this->assertEquals('78ac0e14-0f2b-529e-81e2-a0f50f6029c5', $actual);
     }
 
+    public function testMissingServerVars(): void
+    {
+        $stack    = [
+            $this->getInstance(),
+        ];
+        $response = Dispatcher::run($stack);
+
+        $actual = $response->getHeaderLine('X-Generated-By');
+
+        $this->assertEquals('', $actual);
+    }
+
     private function getInstance(): GeneratedByMiddleware
     {
         $container = new ServiceManager();
